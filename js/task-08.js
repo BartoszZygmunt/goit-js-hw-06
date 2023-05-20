632da4e9fca71ecc443255f61fe2510c7a138d46
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-05-20 14:27:58 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  const myResult = {
+    login: email.value,
+    password: password.value,
+  };
+
+  console.log(myResult);
+  event.currentTarget.reset();
+}
