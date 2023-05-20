1470bff59d812ae86cfd0e6f90c700a9dc8602c7
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-05-20 14:41:36 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+
+const handleClick = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = body.style.backgroundColor;
+};
+
+button.addEventListener("click", handleClick);
