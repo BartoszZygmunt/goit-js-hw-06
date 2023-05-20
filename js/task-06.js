881e79d2b97380b8206f07a5b5834350e2547f90
BartoszZygmunt <it@bartoszzygmunt.pl>
2023-05-20 13:20:34 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,13 @@
+const myInput = document.querySelector("#validation-input");
+
+const handleBlur = () => {
+  if (myInput.value.length >= myInput.dataset.length) {
+    myInput.classList.remove("invalid");
+    myInput.classList.add("valid");
+  } else {
+    myInput.classList.remove("valid");
+    myInput.classList.add("invalid");
+  }
+};
+
+myInput.addEventListener("blur", handleBlur);
